41d68cbbf8022c7e9dd018f322c2404fc4cc7d1f
harmanharinau <107754598+harmanharinau@users.noreply.github.com>
2022-08-02 20:43:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -49,6 +49,7 @@ async def inline_handlers(_, event: Message):
     if event.text == '/start':
         return
     answers = f'**📂 Results For ➠ {event.text} \n\n▰▱▰▱▰▱▰▱▰▱▰▱▰▱\n➠ Type Only Movie Name With Correct Spelling.✍️\n➠ Add Year For Better Result.🗓️\n▰▱▰▱▰▱▰▱▰▱▰▱▰▱\n\n**'
+    async for message in User.search_messages(chat_id=Config.CHANNEL_ID, limit=50, query=event.text):
         if message.text:
             thumb = None
             f_text = message.text
